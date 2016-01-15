45caf634da3e469b7233b6ae4066ca69b8b8c679
Fang Yang <fyang223@wisc.edu>
2016-01-15 13:49:52 -0600

    moving tests to other file

--- stats.py
+++ stats.py
@@ -2,8 +2,17 @@ def mean(vals):
 	assert type(vals) is list
 	total = sum(vals)
 	length = len(vals)
-	return total/length
+	if length == 0:
+		return 0.0
+	else:
+		return total/length
+	
 def test_mean():
 	assert mean([2,4]) == 3.0
 test_mean()
-print(mean([2,4]))
+
+def test_empty_list():
+	assert mean([]) == 0.0
+test_empty_list()
+
+print(mean([]))
