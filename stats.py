ed7b0f9b01f53c6b4c72f94f3e0a8c8b90efa792
Fang Yang <fyang223@wisc.edu>
2016-01-15 13:28:38 -0600

    adding real function

--- stats.py
+++ stats.py
@@ -1,5 +1,9 @@
 def mean(vals):
+	assert type(vals) is list
 	total = sum(vals)
 	length = len(vals)
 	return total/length
+def test_mean():
+	assert mean([2,4]) == 3.0
+test_mean()
 print(mean([2,4]))
